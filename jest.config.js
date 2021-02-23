ad8a2616d718a1ee1f24397c144c16748214ed8a
Ali Habibzadeh <ali.hci@icloud.com>
2021-02-23 19:01:01 +0000

    template binding

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,14 @@
 module.exports = {
   testMatch: ["<rootDir>/src/**/*.test.ts"],
-  reporters: ["default", "<rootDir>/dist/index.js"],
+  reporters: [
+    "default",
+    [
+      "<rootDir>/dist/index.js",
+      {
+        projectName: "@deepcrawl/branded-jest-html-reporter",
+        projectDescription: "A DeepCrawl branded Jest Html Reporter",
+        repositoryUrl: "https://github.com/deepcrawl/branded-jest-html-reporter",
+      },
+    ],
+  ],
 };
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,40 @@
+import { promises as fs } from "fs";
+import { join } from "path";
+
 import { Reporter, Context, Config } from "@jest/reporters";
 import { AggregatedResult } from "@jest/test-result";
+import { renderFile } from "ejs";
+import { groupBy } from "ramda";
 
 interface IBrandedHtmlReporterOptions {
-  foo: string;
+  outputDir?: string;
+  outputFile?: string;
+  projectName?: string;
+  repositoryUrl?: string;
+  projectDescription?: string;
 }
 
 export default class BrandedHtmlReporter implements Pick<Reporter, "onRunComplete"> {
   constructor(private globalConfig: Config.GlobalConfig, private options: IBrandedHtmlReporterOptions) {}
 
   public async onRunComplete(_: Set<Context>, results: AggregatedResult): Promise<void> {
-    // eslint-disable-next-line no-console
-    console.log(JSON.stringify(results));
+    const suites = results.testResults.map(res => ({
+      ...res,
+      testResults: groupBy(item => item.ancestorTitles[0], res.testResults),
+    }));
+    const template = await this.getRenderedTemplate({ ...this.options, suites });
+    await this.writeReport(template);
+  }
+
+  private async writeReport(renderedTemplate: string): Promise<void> {
+    return fs.writeFile(this.getWritingPath(), renderedTemplate);
+  }
+
+  private getRenderedTemplate(templateData: Record<string, unknown>): Promise<string> {
+    return renderFile(join(__dirname, "report.html"), templateData);
+  }
+
+  private getWritingPath(): string {
+    return `${this.options.outputDir ?? "."}/${this.options.outputFile ?? "report.html"}`;
   }
 }
